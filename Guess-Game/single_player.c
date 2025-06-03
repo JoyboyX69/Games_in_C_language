f329f990baeb14f7112d5331c7984129f4c69bc6
Ankush <psrankush@gmail.com>
2025-06-03 19:16:03 +0530

    games are ready to use

---
+++ Guess-Game/single_player.c
@@ -0,0 +1,53 @@
+// Question: Write a program to create a game name is guess the number and also show how many times to find the number
+
+// header file
+#include<stdio.h>
+// this is standard library header file to pick the random number by computer 
+#include<stdlib.h>
+// in this library header life its help in generating different random number every time when the program is executed 
+#include<time.h>
+
+// main file
+int main()
+{   
+    // its help to generate the random number 
+    srand(time(0));
+
+    // Take vairable for store the random nuber ,input the number by user,and how many time you guessed the number
+    int random_number,guesses_of_number=0,guessed;
+
+    // it helps to generate the number between 1 to 100
+    random_number=(rand()%100)+1;
+
+    // printf("Random no is %d",random_number);
+
+    // start a do while loop because atleast take one input by the user
+        do
+        {
+            // take an input by user
+            printf("\nGuess the number: ");
+            scanf("%d",&guessed);
+            
+            // condition start if guess number is greater than random number then print pick lower number
+            if(guessed>random_number)
+            printf("Pick Lower Number");
+
+            // if guess is less the random number then print pick higher number
+            else if(guessed<random_number)
+            printf("Pick Higher Number");
+
+            // otherwise print you guessed the number
+            else
+            printf("Congratulations You Guessed a Number\n");
+
+            // how many times you guessed the number
+            guesses_of_number++;
+        }
+        // conditoin is applicable when guessed the number is not equal to random_number
+        while(guessed != random_number);
+
+        // print the how many times to take guessed the number 
+        printf("You guesses a number in the %d times",guesses_of_number);
+    return 0;
+    // end of code
+}
